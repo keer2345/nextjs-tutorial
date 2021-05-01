94e6ad54cb844d2552a45dca1db28cdaae8ba804
keer2345 <keer2345@gmail.com>
2021-05-01 09:34:43 +0800

    Adding Other Components

---
+++ components/footer.js
@@ -0,0 +1,4 @@
+const Footer = () => {
+  return <div>Copyright &copy; 2021 Keer Next.js</div>
+}
+export default Footer
---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Keer List</h1>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Keer Listing</a>
+    </nav>
+  )
+}
+export default Navbar
