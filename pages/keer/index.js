607799ceca815877bcb54371bc1e293828a7cde1
keer2345 <keer2345@gmail.com>
2021-05-02 19:28:35 +0800

    Fetching Data (getStaticProps)

--- pages/keer/index.js
+++ pages/keer/index.js
@@ -1,7 +1,24 @@
-const Keer = () => {
+import styles from '../../styles/Keer.module.css'
+
+export const getStaticProps = async () => {
+  const res = await fetch('http://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  return {
+    props: { lists: data }
+  }
+}
+const Keer = ({ lists }) => {
   return (
     <div>
-      <h1>All Keer </h1>
+      <h1>All Lists </h1>
+      {lists.map((list) => (
+        <div key={list.id}>
+          <a className={styles.single}>
+            <h3>{list.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   )
 }
