4c3d7ef4ca511df88e51d21844d556c2ee6a34a0
keer2345 <keer2345@gmail.com>
2021-05-02 20:43:03 +0800

    Fetching a Single Item

--- pages/keer/[id].js
+++ pages/keer/[id].js
@@ -1,7 +1,35 @@
-const Details = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch('http://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  const paths = data.map((list) => {
+    return {
+      params: { id: list.id.toString() }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+  const data = await res.json()
+
+  return {
+    props: { keer: data }
+  }
+}
+const Details = ({ keer }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{keer.name}</h1>
+      <p>{keer.email}</p>
+      <p>{keer.website}</p>
+      <p>{keer.address.city}</p>
     </div>
   )
 }
